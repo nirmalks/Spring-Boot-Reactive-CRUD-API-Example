f398d92bd2fca250531130df96d425c89dc0e537
Nirmal Kumar Sekar <kagosekar@gmail.com>
2017-12-19 08:13:47 +0530

    Added model and repository

---
+++ src/main/java/com/example/demo/model/Cricketer.java
@@ -0,0 +1,55 @@
+package com.example.demo.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="cricketer")
+public class Cricketer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	
+	private String country;
+	
+	private String highestScore;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getHighestScore() {
+		return highestScore;
+	}
+
+	public void setHighestScore(String highestScore) {
+		this.highestScore = highestScore;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/demo/repository/CricketerRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Cricketer;
+
+@Repository
+public interface CricketerRepository extends ReactiveCrudRepository<Cricketer , Long>{
+
+}
